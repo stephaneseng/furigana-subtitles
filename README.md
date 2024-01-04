5484fba482d9a77d7eef6f682b5c9f3de7b82c2c
Stephane Seng <seng.stephane@gmail.com>
2024-01-04 19:39:58 +0100

    YouTube navigation support.

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 ## References
 
 * https://iandevlin.com/blog/2015/02/javascript/dynamically-adding-text-tracks-to-html5-video/
-* https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
 * https://stackoverflow.com/questions/53957679/js-create-ruby-tags-from-japanese-kana-input
--- timed-furigana.js
+++ timed-furigana.js
@@ -1,49 +1,55 @@
-const CONFIGURATION = [
-    {
-        // Hanaichi Monnme
-        videoId: 'H88kps8X4Mk',
-        cues: [
-            {
-                line: 1,
-                start: 19.600,
-                end: 22.666,
-                translations: [
-                    '真夜中 midnight',
-                    '溢 to let (one\'s feelings) show',
-                    '午前 a.m.',
-                    '時 o\'clock'
-                ],
-                furiganas: [
-                    '真 ま',
-                    '夜 よ',
-                    '中 なか',
-                    '溢 こぼ',
-                    '午 ご',
-                    '前 ぜん',
-                    '時 じ'
-                ]
-            },
-            {
-                line: 2,
-                start: 22.700,
-                end: 26.533,
-                translations: [
-                    '目に見え to be visible',
-                    '波形 waveform',
-                    '腐 to rot'
-                ],
-                furiganas: [
-                    '目 め',
-                    '見 み',
-                    '波 は',
-                    '形 けい',
-                    '腐 くさ'
-                ]
-            }
-        ]
-    }
+const SUPPORTED_VIDEO_IDS = [
+    'H88kps8X4Mk' // 2023-06-04 Hanaichi Monnme
 ];
 
+function main() {
+    console.debug('Extension loaded');
+
+    const videoId = (new URLSearchParams(window.location.search)).get('v');
+
+    if (!SUPPORTED_VIDEO_IDS.includes(videoId)) {
+        console.debug('Video not supported');
+        return;
+    }
+
+    import(browser.extension.getURL(`/configurations/${videoId}.js`)).then(configuration => {
+        console.debug('Configuration file loaded');
+
+        waitForElement('video').then(videoElement => {
+            console.debug('Video element found');
+
+            for (const textTrack of videoElement.textTracks) {
+                textTrack.mode = 'disabled';
+            }
+
+            waitForElement('#comments #pinned-comment-badge').then(pinnedCommentBadgeElement => {
+                console.debug('Pinned comment found');
+
+                const lines = pinnedCommentBadgeElement.closest('#main').querySelector('#content-text').innerText.split(/\r\n/g);
+
+                const track = videoElement.addTextTrack('captions', 'Japanese', 'jp');
+                track.mode = 'showing';
+
+                for (const cue of configuration.cues) {
+                    var line = lines[cue.line - 1];
+
+                    cue.kanjis.forEach(kanji => {
+                        const left = kanji.substring(0, kanji.indexOf(' '));
+                        const right = kanji.substring(kanji.indexOf(' ') + 1);
+
+                        line = line.replace(left, `<ruby>${left}<rt>${right}</rt></ruby>`);
+                    });
+
+                    track.addCue(new VTTCue(cue.start, cue.end, line));
+                }
+
+                console.debug('Cues added');
+            });
+        });
+    });
+}
+
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
 function waitForElement(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -64,45 +70,5 @@ function waitForElement(selector) {
     });
 }
 
-console.debug('Extension loaded');
-
-const urlSearchParams = new URLSearchParams(window.location.search);
-const videoId = urlSearchParams.get('v');
-
-const configuration = CONFIGURATION.filter(c => c.videoId === videoId)[0];
-
-if (configuration !== undefined) {
-    waitForElement('video').then(videoElement => {
-        console.debug('Video element found');
-
-        waitForElement('#comments #pinned-comment-badge').then(pinnedCommentBadgeElement => {
-            console.debug('Pinned comment found');
-
-            const lines = pinnedCommentBadgeElement.closest('#main').querySelector('#content-text').innerText.split(/\r\n/g);
-            console.debug(lines);
-
-            const track = videoElement.addTextTrack('captions', 'Japanese', 'jp');
-            track.mode = 'showing';
-
-            configuration.cues.forEach(cue => {
-                var line = lines[cue.line - 1];
-
-                cue.translations.forEach(translation => {
-                    const left = translation.substring(0, translation.indexOf(' '));
-                    const right = translation.substring(translation.indexOf(' ') + 1);
-
-                    line = line.replace(left, `<ruby style="ruby-position: under;">${left}<rt>${right}</rt></ruby>`);
-                });
-
-                cue.furiganas.forEach(furigana => {
-                    const left = furigana.substring(0, furigana.indexOf(' '));
-                    const right = furigana.substring(furigana.indexOf(' ') + 1);
-
-                    line = line.replace(left, `<ruby>${left}<rt>${right}</rt></ruby>`);
-                });
-
-                track.addCue(new VTTCue(cue.start, cue.end, line));
-            });
-        })
-    });
-}
+// https://stackoverflow.com/questions/34077641/how-to-detect-page-navigation-on-youtube-and-modify-its-appearance-seamlessly
+document.addEventListener('yt-navigate-finish', main);
