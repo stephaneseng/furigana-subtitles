69c0eccb080b8fff8048dad4b536c0bd3e1d11ee
Stephane Seng <seng.stephane@gmail.com>
2024-01-04 17:59:40 +0100

    Experiment with english translations.

--- timed-furigana.js
+++ timed-furigana.js
@@ -1,32 +1,43 @@
 const CONFIGURATION = [
     {
-        // ZUTOMAYO – Hanaichi Monnme (Music Video)
+        // Hanaichi Monnme
         videoId: 'H88kps8X4Mk',
         cues: [
             {
                 line: 1,
                 start: 19.600,
                 end: 22.666,
+                translations: [
+                    '真夜中 midnight',
+                    '溢 to let (one\'s feelings) show',
+                    '午前 a.m.',
+                    '時 o\'clock'
+                ],
                 furiganas: [
-                    { kanji: '真', kana: 'ま' },
-                    { kanji: '夜', kana: 'よ' },
-                    { kanji: '中', kana: 'なか' },
-                    { kanji: '溢', kana: 'こぼ' },
-                    { kanji: '午', kana: 'ご' },
-                    { kanji: '前', kana: 'ぜん' },
-                    { kanji: '時', kana: 'じ' }
+                    '真 ま',
+                    '夜 よ',
+                    '中 なか',
+                    '溢 こぼ',
+                    '午 ご',
+                    '前 ぜん',
+                    '時 じ'
                 ]
             },
             {
                 line: 2,
                 start: 22.700,
                 end: 26.533,
+                translations: [
+                    '目に見え to be visible',
+                    '波形 waveform',
+                    '腐 to rot'
+                ],
                 furiganas: [
-                    { kanji: '目', kana: 'め' },
-                    { kanji: '見', kana: 'み' },
-                    { kanji: '波', kana: 'は' },
-                    { kanji: '形', kana: 'けい' },
-                    { kanji: '腐', kana: 'くさ' }
+                    '目 め',
+                    '見 み',
+                    '波 は',
+                    '形 けい',
+                    '腐 くさ'
                 ]
             }
         ]
@@ -76,8 +87,18 @@ if (configuration !== undefined) {
             configuration.cues.forEach(cue => {
                 var line = lines[cue.line - 1];
 
+                cue.translations.forEach(translation => {
+                    const left = translation.substring(0, translation.indexOf(' '));
+                    const right = translation.substring(translation.indexOf(' ') + 1);
+
+                    line = line.replace(left, `<ruby style="ruby-position: under;">${left}<rt>${right}</rt></ruby>`);
+                });
+
                 cue.furiganas.forEach(furigana => {
-                    line = line.replace(furigana.kanji, `<ruby>${furigana.kanji}<rt>${furigana.kana}</rt></ruby>`)
+                    const left = furigana.substring(0, furigana.indexOf(' '));
+                    const right = furigana.substring(furigana.indexOf(' ') + 1);
+
+                    line = line.replace(left, `<ruby>${left}<rt>${right}</rt></ruby>`);
                 });
 
                 track.addCue(new VTTCue(cue.start, cue.end, line));
